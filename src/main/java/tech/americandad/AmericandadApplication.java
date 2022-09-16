afca845823bbd1db4d09f2d08a5168e59a924799
Jamerson Souza <jamersonsouza@outlook.com>
2022-09-15 22:36:52 -0300

    Inicio do Projeto e Criação das classes de dominio

---
+++ src/main/java/tech/americandad/AmericandadApplication.java
@@ -0,0 +1,13 @@
+package tech.americandad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericandadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericandadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/americandad/AmericandadApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.americandad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericandadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
