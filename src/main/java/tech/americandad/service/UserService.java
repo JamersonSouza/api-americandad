63b3ae685eae66dabdc5a9a2fa81768616b84c3e
Jamerson Souza <jamersonsouza@outlook.com>
2022-10-16 15:59:58 -0300

    Métodos para registro, listagem de usuario e busca

--- src/main/java/tech/americandad/service/UserService.java
+++ src/main/java/tech/americandad/service/UserService.java
@@ -1,5 +1,20 @@
 package tech.americandad.service;
 
+import java.util.List;
+
+import tech.americandad.domain.User;
+import tech.americandad.exceptions.domain.EmailExistsException;
+import tech.americandad.exceptions.domain.UsuarioExistsException;
+import tech.americandad.exceptions.domain.UsuarioNotFoundException;
+
 public interface UserService {
+
+    User registro(String nome, String sobrenome, String usuario, String email) throws EmailExistsException, UsuarioExistsException, UsuarioNotFoundException;
+
+    List<User> listUsers();
+
+    User findUserByUsuario(String usuario);
+
+    User findUserByEmail(String email);
     
 }
