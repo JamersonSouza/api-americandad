8255d63561486a2edb00dbcd3900c4d93186b955
Jamerson Souza <jamersonsouza@outlook.com>
2022-10-23 10:51:46 -0300

    Variaveis para o envio de email

---
+++ src/main/java/tech/americandad/constants/EmailConstant.java
@@ -0,0 +1,20 @@
+package tech.americandad.constants;
+
+public class EmailConstant {
+
+    public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
+    public static final String USUARIO = "americandademailteste@gmail.com";
+    public static final String PASSWORD = "dc@S4M$bSdH$";
+    public static final String FROM_EMAIL = "contato@jovemapostador.com.br";
+    public static final String CC_EMAIL = "";
+    public static final String EMAIL_ASSUNTO = "Curso Spring Boot API com Angular";
+    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smpt.port";
+    public static final int DEFAULT_PORT = 465;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+
+    
+}
