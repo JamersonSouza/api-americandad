867f192f82eccf05c218ee325865d04aa5fe2245
Jamerson Souza <jamersonsouza@outlook.com>
2022-10-31 22:09:10 -0300

    Constantes para erros de email/usuario

---
+++ src/main/java/tech/americandad/constants/UserImpConstants.java
@@ -0,0 +1,12 @@
+package tech.americandad.constants;
+
+public class UserImpConstants {
+
+
+    public static final String USERNAME_ALREADY_EXISTS = "Username já cadastrado";
+    public static final String EMAIL_ALREADY_EXISTS = "Email já cadastrado";
+    public static final String NO_USER_FOUND_BY_USERNAME = "Nenhum usuário encontrado para: ";
+    public static final String FOUND_USER_BY_USERNAME = "Retornando usuário encontrado por nome de usuário: ";
+    public static final String NO_USER_FOUND_BY_EMAIL = "Nenhum e-mail encontrado para:  ";
+    
+}
